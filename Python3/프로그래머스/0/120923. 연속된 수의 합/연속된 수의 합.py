c714152c0114b482b2ab2f4303b3f77b79592936
stjoo0925 <stjoo0925@gmail.com>
2025-01-23 00:11:51 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ Python3/프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,5 +1,10 @@
 def solution(num, total):
-    start = (total - (num * (num - 1) // 2)) // num
+    y = num // 2
+    if total % num == 0:
+        x = total // num
+    else:
+        x = total // num + 1
+    start = x - y
     answer = []
     for i in range(0, num):
         answer.append(start + i)
